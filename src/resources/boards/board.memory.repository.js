3ac2944f036ab43b2df91698fc9acf803bf1d885
unknown <olga.qa@yahoo.com>
2021-05-30 00:16:01 +0300

    doc: add jsdoc for Board component

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -1,11 +1,46 @@
+/**
+ * @module BoardRepository
+ */
+
 const dB = require('../../common/inMemoryDb');
 
+/**
+ * Returns all the boards
+ * @async
+ * @returns {Promise<Array<Board>>} Promise which resolves to array of boards
+ */
 const getAll = async () => dB.getAllBoards();
+
+/**
+ * Returns board with specified id from DB
+ * @async
+ * @param {uuid} id unique identifier for board to look for
+ * @returns {Promise<Board|undefined>} Promise which resolves to found board or undefined (if no such board found)
+ */
 const get = async (id) => dB.getBoard(id);
+
+/**
+ * Creates board in DB
+ * @async
+ * @param {Board} board board to create in db
+ * @returns {Promise<Board>} Promise which resolves to created board
+ */
 const create = async (board) => dB.createBoard(board);
 
-const remove = async (board) => dB.removeBoard(board);
+/**
+ * Removes board from DB
+ * @async
+ * @param {uuid} boardId unique identifier of board to remove
+ * @returns {Promise<undefined>} Promise which resolves to undefined
+ */
+const remove = async (boardId) => dB.removeBoard(boardId);
 
+/**
+ * Updates board with specified id
+ * @async
+ * @param {Board} board board to update with new data
+ * @returns {Promise<Board>} Promise which resolves to updated board
+ */
 const update = async (board) => dB.updateBoard(board);
 
 module.exports = { getAll, get, create, update, remove };
--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,7 +1,15 @@
 const uuid = require('uuid');
 const Column = require('../columns/column.model');
 
+/** Class representing a Board */
 class Board {
+  /**
+   * Create a board
+   * @param {Object} boardData
+   * @param {uuid} [boardData.id=uuid.v4()] unique identifier for board
+   * @param {string} [boardData.title=Board] title for board
+   * @param {Array<Column>} [boardData.columns=[]] columns of the board
+   */
   constructor({ id = uuid.v4(), title = 'Board', columns = [] } = {}) {
     this.id = id;
     this.title = title;
