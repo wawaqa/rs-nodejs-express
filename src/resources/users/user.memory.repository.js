49bef730dce3a443f94cd855637787cb35307045
unknown <olga.qa@yahoo.com>
2021-05-29 21:44:55 +0300

    doc: add jsdoc for User component

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,11 +1,47 @@
 const dB = require('../../common/inMemoryDb');
 
+/**
+ * @typedef {require('./user.model').User} User
+ */
+
+
+/**
+ * Returns all the users from DB
+ * @async
+ * @returns {Promise<Array<User>>} Promise which resolves to array of users
+ */
 const getAll = async () => dB.getAllUsers();
+
+/**
+ * Returns user with specified id from DB
+ * @async
+ * @param {uuid} id unique identifier for user to look for
+ * @returns {Promise<User|undefined>} Promise which resolves to found user or undefined (if no such user found)
+ */
 const get = async (id) => dB.getUser(id);
+
+/**
+ * Creates user in DB
+ * @async
+ * @param {User} user user to create in db
+ * @returns {Promise<User>} Promise which resolves to createdUser
+ */
 const create = async (user) => dB.createUser(user);
 
+/**
+ * Removes user from DB
+ * @async
+ * @param {uuid} id unique identifier of user to remove
+ * @returns {Promise<undefined>} Promise which resolves to undefined
+ */
 const remove = async (id) => dB.removeUser(id);
 
+/**
+ * Updates user with specified id
+ * @async
+ * @param {User} user user to update with new data
+ * @returns {Promise<User>}
+ */
 const update = async (user) => dB.updateUser(user);
 
 module.exports = { getAll, get, create, update, remove };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,15 +1,40 @@
 const usersRepo = require('./user.memory.repository');
 const User = require('./user.model');
 
+/**
+ * @typedef {require('./user.model').UserDTO} UserDTO
+ */
+
+/**
+ * Returns all the users as user data-transfer-objects (without passwords)
+ * @async
+ * @returns {Promise<Array<UserDTO>>} Promise which resolves to array of user data-transfer-objects
+ */
 const getAll = async () => {
   const users = await usersRepo.getAll();
   return users.map(User.toResponse);
 };
+/**
+ * Returns user with specified id as data-transfer-object (without password)
+ * @async
+ * @param {uuid} id unique identifier for user to look for
+ * @returns {Promise<UserDTO|undefined>} Promise which resolves to found user or undefined (if no such user found)
+ */
 const get = async (id) => {
   const user = await usersRepo.get(id);
   if (user) return User.toResponse(user);
   return undefined;
 };
+
+/**
+ * Creates user and returns created user as data-transfer-object
+ * @async
+ * @param {Object} userData data to create user
+ * @param {string} userData.name name for user to create
+ * @param {string} userData.login login for user to create
+ * @param {string} userData.password password for user to create
+ * @returns {Promise<UserDTO>} Promise which resolves to data-transfer-object for created user
+ */
 const create = async (userData) => {
   const { name, login, password } = userData;
   const newUser = new User({ name, login, password });
@@ -17,12 +42,29 @@ const create = async (userData) => {
   return User.toResponse(createdUser);
 };
 
-const update = (userData) => {
+/**
+ * Creates user and returns created user as data-transfer-object
+ * @async
+ * @param {Object} userData new user data for user to update
+ * @param {uuid} userData.id unique identifier for user to update
+ * @param {string} userData.name name for user to update
+ * @param {string} userData.login login for user to update
+ * @param {string} userData.password password for user to update
+ * @returns {Promise<UserDTO>} Promise which resolves to updated user as data-transfer-object
+ */
+const update = async (userData) => {
   const { name, login, password, id } = userData;
   const user = new User({ name, login, password, id });
   const updatedUser = usersRepo.update(user);
   return User.toResponse(updatedUser);
 };
-const remove = (id) => usersRepo.remove(id);
+
+/**
+ * Removes user with specified id
+ * @async
+ * @param {uuid} id unique udentifier for user to remove
+ * @returns {Promise<undefined>} Promise which resolves to undefined (if no such user found)
+ */
+const remove = async (id) => usersRepo.remove(id);
 
 module.exports = { getAll, get, create, update, remove };
