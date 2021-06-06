9efe8b6ec1907da8d244e7c1bd60b71d358b035e
unknown <olga.qa@yahoo.com>
2021-06-06 18:58:55 +0300

    fix: to add data to file with process exit

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,7 +1,7 @@
 import { ILogData, convertToLogRecord } from './dataLog.js';
 import { writeToLog } from './writeToLog.js';
 
-export function logIt(logData: ILogData): void {
+export function logIt(logData: ILogData, exitOnFinishCode?: number): void {
   const logRecord = convertToLogRecord(logData);
-  writeToLog(logRecord);
+  writeToLog(logRecord, exitOnFinishCode);
 }
