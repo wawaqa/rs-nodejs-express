f44c3a4f9b1378b29a4bd140543f02510d7b1bcb
unknown <olga.qa@yahoo.com>
2021-06-17 17:08:03 +0300

    feat: add typeorm config and dependencies

---
+++ ormconfig.ts
@@ -0,0 +1,14 @@
+const typeOrmConfig = {
+  type: 'postgres',
+  host: 'postgres',
+  port:  process.env['POSTRES_PORT'],
+  username: process.env['POSTGRES_USER'],
+  password: process.env['POSTGRES_PASSWORD'],
+  database:  process.env['POSTGRES_DB'],
+  synchronize: true,
+  logging: false,
+  // entities: ['src/entity/**/*.ts'],
+  // migrations: ['src/migration/**/*.ts'],
+  // subscribers: ['src/subscriber/**/*.ts'],
+};
+export default typeOrmConfig;
