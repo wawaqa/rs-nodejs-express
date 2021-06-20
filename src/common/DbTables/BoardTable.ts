45edf777d3d2e3a4e5d0962545f3772db27de682
someqa <olgasomeqa@gmail.com>
2021-06-20 14:31:56 +0300

    feat: remove in-memory db

--- src/common/DbTables/BoardTable.ts
+++
@@ -1,6 +0,0 @@
-import Board from '../../resources/boards/board.entity';
-import Table from './Table';
-
-class BoardTable extends Table<Board> {}
-
-export default BoardTable;
--- src/common/DbTables/UserTable.ts
+++
@@ -1,6 +0,0 @@
-import User from '../../resources/users/user.entity';
-import Table from './Table';
-
-class UserTable extends Table<User> {}
-
-export default UserTable;
